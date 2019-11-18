8fc394e9fbbe0b81b454540bf006e13d1fc25f0c
Mostafa Biomee <m.biomee@gmail.com>
2019-11-18 10:23:18 -0600

    implement messages

---
+++ Message.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Biomee.Infra.Messaging
+{
+    public class Message
+    {
+        public readonly Guid MessageId;
+        public readonly string MessageType;
+
+        public Message() : this(Guid.NewGuid())
+        {
+        }
+
+        public Message(Guid messageId)
+        {
+            MessageId = messageId;
+            MessageType = this.GetType().Name;
+        }
+
+        public Message(string messageType) : this(Guid.NewGuid())
+        {
+            MessageType = messageType;
+        }
+
+        public Message(Guid messageId, string messageType)
+        {
+            MessageId = messageId;
+            MessageType = messageType;
+        }
+    }
+}
