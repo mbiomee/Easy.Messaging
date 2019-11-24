aa7e8038228ea7b2820329549f7bbd0519f6dd40
Mostafa Biomee <m.biomee@gmail.com>
2019-11-24 13:47:53 -0600

    update Exception

---
+++ Configuration/InvalidConfigurationException.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Biomee.Infra.Messaging.Configuration
+{
+    public class InvalidConfigurationException : Exception
+    {
+        public InvalidConfigurationException()
+        {
+        }
+
+        public InvalidConfigurationException(string message) : base(message)
+        {
+        }
+
+        public InvalidConfigurationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
